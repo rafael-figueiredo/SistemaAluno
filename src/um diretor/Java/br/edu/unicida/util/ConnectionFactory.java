cf9d15cb35a993492bfb5851350be76d79b473b4
Rafael Figueiredo Rodrigues <rafael.figueiredo.2000@hotmail.com>
2021-10-31 13:43:20 -0300

    Create ConnectionFactory.java

---
+++ src/um diretor/Java/br/edu/unicida/util/ConnectionFactory.java
@@ -0,0 +1,48 @@
+package br.edu.unicid.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ConnectionFactory {
+
+	public static Connection getConnection() throws Exception {
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+
+			return DriverManager.getConnection("jdbc:mysql://localhost:3306/sisaluno", "root", "");
+
+		} catch (Exception erro) {
+			throw new Exception(erro.getMessage());
+		}
+
+	}
+
+	public static void closeConnection(Connection conn, Statement stmt, ResultSet rs) throws Exception {
+		close(conn, stmt, rs);
+	}
+	
+	public static void closeConnection(Connection conn, Statement stmt) throws Exception {
+		close(conn, stmt, null);
+	}
+	
+	public static void closeConnection(Connection conn) throws Exception {
+		close(conn, null, null);
+	}
+
+	private static void close(Connection conn, Statement stmt, ResultSet rs) throws Exception {
+		try {
+			if (rs != null)
+				rs.close();
+			if (stmt != null)
+				stmt.close();
+			if (conn != null)
+				conn.close();
+
+		} catch (Exception erro) {
+
+		}
+	};
+}
